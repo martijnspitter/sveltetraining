3f5a0a39c45806ed8a23bf9ca3cb19d9d01ea804
Martijn Spitter <info@martijnspitter.nl>
2021-06-20 22:30:17 +0200

    added event dispatcher

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,12 @@
 <script>
 	import HowTo from './HowTo.svelte';
 	import Timer from './Timer.svelte';
+
+	let audio;
+
+	function timerEnds() {
+		audio.play()
+	}
 </script>
 
 <style>
@@ -10,8 +16,11 @@
 </style>
 
 <h1>Handwashing App</h1>
-<Timer />
+<Timer on:end={timerEnds}/>
 
 <HowTo />
 <h3>Source</h3>
-
+<audio bind:this={audio}>
+	<source src="sound.wav" />
+	<track kind="captions">
+</audio>
